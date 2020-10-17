148899396efbd1bf0d775ee310d5e975ac7344cb
ammaar345 <fakirammaar8@gmail.com>
2020-10-17 19:24:08 +0200

    created table , binding all three datasources

--- waiter.sql
+++ waiter.sql
@@ -1,2 +1,38 @@
-create table waiters();
-create table weekdays();
+create table waiters
+(
+    id serial not null primary key,
+    name text
+);
+create table weekdays
+(
+    id serial not null primary key,
+    dayofweek text,
+    weekdayid int,
+    FOREIGN key(weekdayid) references waiters(id)
+);
+create table tblshift
+(
+    id serial not null primary key,
+    weekdaysid int,
+    FOREIGN key (weekdaysid) REFERENCES waiters(id)
+)
+INSERT INTO weekdays
+    ( [dayofweek])
+VALUES
+    ( "Monday")
+INSERT INTO weekdays
+    ( [dayofweek])
+VALUES
+    ( "Tuesday")
+INSERT INTO weekdays
+    ([dayofweek])
+VALUES
+    ( "Wednesday")
+INSERT INTO weekdays
+    ( [dayofweek])
+VALUES
+    ( "Thursday")
+INSERT INTO weekdays
+    ([dayofweek] )
+VALUES
+    ( "Friday")
