f4787b18c92e33d778e2ea7b26471657265af7de
ammaar345 <fakirammaar8@gmail.com>
2020-10-22 01:13:14 +0200

    added an insert function

--- waiter.js
+++ waiter.js
@@ -1,17 +1,28 @@
 module.exports = function WaiterFunc(pool) {
 
-    async function addUser(user) {
-        INSERT_QUERY = "INSERT into "
+    async function addUser(user,weekday) {
+        const SELECT_QUERY='SELECT FROM waiters where name=$1';
+        const waiter=await pool.query(SELECT_QUERY,[user]).rows.length
+        const INSERT_QUERY = "INSERT into waiters values ($1,$2) "
+        const UPDATE_QUERY="UPDATE tblshift set weekdayid=$2 where waiternameid=$3" //$3 = id parameter
+if (waiter>0){
+await pool.query(UPDATE_QUERY,[weekday])
+}
+else{
+    await pool.query(INSERT_QUERY,[user,weekday])
+}
 
 
     }
 
     async function clearDataBase() {
         const DELETE_QUERY = 'DELETE FROM waiters '
-
+const clearDb='DELETE FROM tblshift'
+await pool.query(DELETE_QUERY)
+await pool.query(clearDb)
     }
     async function days() {
-        const selectQ = "SELECT dayofweek from weekdays";
+        const selectQ = 'SELECT dayofweek from weekdays';
         const days = await pool.query(selectQ);
         console.log(days.rows)
         return days.rows
